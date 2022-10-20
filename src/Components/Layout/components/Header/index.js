f749c4c75b3d05f4c79aa4f71cbf44e4fb69db1e
Viet Truong <viettruong0825@gmail.com>
2022-10-20 08:30:30 +0700

    config svg file

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -1,12 +1,17 @@
 import classNames from "classnames/bind";
 import styles from "./Header.module.scss";
+import images from "../../../../assets/images";
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx("wrapper")}>
-            <div className={cx("inner")}></div>
+            <div className={cx("inner")}>
+                <div className={cx("logo")}>
+                    <img src={images.logo} alt="top top" />
+                </div>
+            </div>
         </header>
     );
 }
