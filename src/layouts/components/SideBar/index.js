559a8e1ef4705a7e4715fcfa9ec7b9d1f0b22f99
Viet Truong <viettruong0825@gmail.com>
2022-11-16 18:06:58 +0700

    fix UI hashtag and make footer

--- src/layouts/components/SideBar/index.js
+++ src/layouts/components/SideBar/index.js
@@ -1,7 +1,9 @@
 import classNames from "classnames/bind";
 import styles from "./SideBar.module.scss";
 import { useState, useEffect } from "react";
+import Tippy from "@tippyjs/react/headless";
 
+import { Wrapper as PopperWrapper } from "../../../Components/Popper";
 import Menu, { MenuItem } from "./Menu";
 import {
     HomeIcon,
@@ -36,11 +38,11 @@ const hashtagItem = [
     },
     {
         icon: <FontAwesomeIcon icon={faMusic} />,
-        text: "Yêu Đơn Phương Là Gì(MEE Remix) - Mee ...",
+        text: "Yêu Đơn Phương Là Gì (MEE Remix) - Mee Media & h0n",
     },
     {
         icon: <FontAwesomeIcon icon={faMusic} />,
-        text: "Về Nghe Mẹ Ru - NSND Bach Tuyet & Hứa ...",
+        text: "Về Nghe Mẹ Ru - NSND Bach Tuyet & Hứa Kim Tuyền & 14 Casper & Hoàng Dũng",
     },
     {
         icon: <FontAwesomeIcon icon={faMusic} />,
@@ -60,7 +62,87 @@ const hashtagItem = [
     },
     {
         icon: <FontAwesomeIcon icon={faMusic} />,
-        text: "Thằng Hầu (Thái Hoàng Remix) [Short Vers...",
+        text: "Thằng Hầu (Thái Hoàng Remix) [Short Version] - Dunghoangpham",
+    },
+];
+
+const footerItems = [
+    {
+        row: [
+            {
+                title: "Giới thiệu",
+            },
+            {
+                title: "Bảng tin",
+            },
+            {
+                title: "Liên Hệ",
+            },
+            {
+                title: "Sự Nghiệp",
+            },
+            {
+                title: "ByteDance",
+            },
+        ],
+    },
+    {
+        row: [
+            {
+                title: "TikTok for Good",
+            },
+            {
+                title: "Quảng cáo",
+            },
+            {
+                title: "Developers",
+            },
+            {
+                title: "Transparency",
+            },
+        ],
+    },
+    {
+        row: [
+            {
+                title: "TikTok Rewards",
+            },
+            {
+                title: "TikTok Browse",
+            },
+            {
+                title: "TikTok Embeds",
+            },
+        ],
+    },
+    {
+        row: [
+            {
+                title: "Trợ giúp",
+            },
+            {
+                title: "An Toàn",
+            },
+            {
+                title: "Điều khoản",
+            },
+            {
+                title: "Quyền riêng tư",
+            },
+        ],
+    },
+    {
+        row: [
+            {
+                title: "Creator Portal",
+            },
+            {
+                title: "Hướng dẫn",
+            },
+            {
+                title: "Cộng đồng",
+            },
+        ],
     },
 ];
 
@@ -68,6 +150,16 @@ function SideBar() {
     const currentUser = true;
     const [followedUser, setFollowedUser] = useState([]);
     const [suggestedUser, setSuggestedUser] = useState([]);
+
+    const renderPreview = () => {
+        return (
+            <PopperWrapper className={cx("popover-more")}>
+                <p className={cx("popover-btn-text")}>
+                    NGUYÊN TẮC THỰC THI PHÁP LUẬT CỦA TIKTOK
+                </p>
+            </PopperWrapper>
+        );
+    };
 
     useEffect(() => {
         const fetchSuggest = async () => {
@@ -135,24 +227,56 @@ function SideBar() {
                 />
 
                 <section className={cx("wrapper-hashtag")}>
-                    <p className={cx("title-wrapper")}>Khám phá</p>
+                    <p className={cx("title-wrapper-hashtag")}>Khám phá</p>
                     <div className={cx("dflex")}>
-                        {hashtagItem.map((item) => (
-                            <a href={"#"} className={cx("href-hashtag")}>
+                        {hashtagItem.map((item, index) => (
+                            <a
+                                href={"#"}
+                                className={cx("href-hashtag")}
+                                key={index}
+                            >
                                 <div className={cx("hashtag")}>
                                     <Button
                                         text
                                         leftIcon={item.icon}
                                         className={cx("btn-hashtag")}
                                     >
-                                        <span className={cx("hashtag-text")}>
+                                        <p className={cx("hashtag-text")}>
                                             {item.text}
-                                        </span>
+                                        </p>
                                     </Button>
                                 </div>
                             </a>
                         ))}
                     </div>
+                </section>
+
+                <section className={cx("wrapper-footer")}>
+                    {footerItems.map((item, index) => (
+                        <div className={cx("wrapper-row")} key={index}>
+                            {item.row.map((item, index) => (
+                                <a className={cx("item")} key={index}>
+                                    {item.title}
+                                </a>
+                            ))}
+                        </div>
+                    ))}
+                    {/* Using a wrapper <div> or <span> tag around the reference element solves this by creating a new parentNode context.  */}
+                    <div>
+                        <Tippy
+                            interactive
+                            delay={[600, 0]}
+                            offset={[0, 10]}
+                            render={renderPreview}
+                            placement="top"
+                        >
+                            <p className={cx("more-btn")}>Thêm</p>
+                        </Tippy>
+                    </div>
+
+                    <p className={cx("copyright")}>
+                        © {""} 2022 TikTok - Code by Viet Truong
+                    </p>
                 </section>
             </div>
         </aside>
