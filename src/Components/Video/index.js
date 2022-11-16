a382a20aa45f3b4bdd2a9ebf125d9c5eff9ae19c
Viet Truong <viettruong0825@gmail.com>
2022-11-16 22:47:42 +0700

    make auto play when scroll visible(bug no play video when first access)

--- src/Components/Video/index.js
+++ src/Components/Video/index.js
@@ -1,43 +1,61 @@
 import classNames from "classnames/bind";
 import styles from "./Video.module.scss";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import VisibilitySensor from "react-visibility-sensor";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles);
 
 function Video({ src }) {
-    const [playing, setPlaying] = useState(false);
     const videoRef = useRef();
+    const [playing, setPlaying] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
+    const handleVideo = () => {
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+    };
+    useEffect(() => {
+        videoRef.current.volume = 0.1;
+        if (isVisible) {
+            videoRef.current.play();
+            setPlaying(true);
+        } else {
+            if (videoRef.current.play) {
+                videoRef.current.pause();
+                setPlaying(false);
+            }
+        }
+    }, [isVisible]);
+
     let toggleIcon = playing ? (
         <FontAwesomeIcon icon={faPause} className={cx("control-icon")} />
     ) : (
         <FontAwesomeIcon icon={faPlay} className={cx("control-icon")} />
     );
-    const handleVideo = () => {
-        if (!playing) {
-            videoRef.current.play();
-            setPlaying(true);
-        } else {
-            videoRef.current.pause();
-            setPlaying(false);
-        }
-    };
     return (
-        <div className={cx("wrapper")}>
-            <div className={cx("wrapper-video")}>
-                <video
-                    className={cx("video")}
-                    src={src}
-                    loop
-                    ref={videoRef}
-                    onClick={handleVideo}
-                />
-                <button className={cx("control")} onClick={handleVideo}>
-                    {toggleIcon}
-                </button>
+        <VisibilitySensor onChange={(isVisible) => setIsVisible(isVisible)}>
+            <div className={cx("wrapper")}>
+                <div className={cx("wrapper-video")}>
+                    <video
+                        className={cx("video")}
+                        src={src}
+                        loop
+                        ref={videoRef}
+                        onClick={handleVideo}
+                    />
+                    <button className={cx("control")} onClick={handleVideo}>
+                        {toggleIcon}
+                    </button>
+                </div>
             </div>
-        </div>
+        </VisibilitySensor>
     );
 }
 
