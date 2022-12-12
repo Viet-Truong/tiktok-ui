43b42e56d106cee9fd2a43609ac12de3e4a2797f
Viet Truong <viettruong0825@gmail.com>
2022-12-12 21:59:35 +0700

    delete componet modal in Search page

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,13 +1,11 @@
 import classNames from "classnames/bind";
 import styles from "./Search.module.scss";
-import Modal from "../../Components/Modal";
 
 const cx = classNames.bind(styles);
 
 function Search() {
     return (
         <div className={cx("wrapper")}>
-            <Modal />
             <h2>Search Page</h2>
         </div>
     );
